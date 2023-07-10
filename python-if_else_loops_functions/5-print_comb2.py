496ed64d644e4b7cfa6e2aa13ba81119d73a3249
Mugara374 <m.mushi@alustudent.com>
2023-07-09 23:26:12 -0700

    Print comb2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if !=99:
-        print("{:d}{:d}, ".format(i // 10, i % 10))
+        print("{:d}{:d}, ".format(i // 10, i % 10), end="")
     else:
         print("{:d}".format(i))
