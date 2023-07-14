08a344b5e778ac57c52d198a5fef568e716d6e80
Mugara374 <m.mushi@alustudent.com>
2023-07-14 03:35:33 -0700

    Printing matrix integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print('{:d}'.format(i))
+    for row in matrix:
+        formatted_row =" ".join('{:4}'.format(num) for num in row)
+        print formatted_row
