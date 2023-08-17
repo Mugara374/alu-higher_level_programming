94971ec247e812d136e862c4cb4191052a92d95f
Mugara374 <m.mushi@alustudent.com>
2023-08-17 08:38:41 -0700

    Almost a circle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,66 +1,187 @@
 #!/usr/bin/python3
-''' models/base.py
-'''
+"""
+This is a python module
+
+that has the class base
+
+This class will be the “base”
+
+of all other classes in this project
+
+The goal of it is to manage id attribute
+
+in all your future classes and to avoid
+
+duplicating the same code (by extension, same bugs)
+"""
+
+
 import json
 
 
 class Base:
-    ''' first class Base
-    '''
+    """
+    This class base has the following;
 
+    private class attribute __nb_objects = 0
+
+    class constructor: def __init__(self, id=None):
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        ''' initialize Base  __init__
-        '''
+        """
+        This is the initialization
+
+        constructor method for each
+
+        instance or object of this class
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        JSON is one of the standard formats for sharing data representation.
+
+        the static method def to_json_string(list_dictionaries)
+
+        that returns the JSON string representation of list_dictionaries
+
+        list_dictionaries is a list of dictionaries
+
+        If list_dictionaries is None or empty, return the string: "[]"
+
+        Otherwise, return the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
     @classmethod
     def save_to_file(cls, list_objs):
-        '''method: save_to_file
-        '''
-        list_dicts = []
-        if list_objs is not None:
-            for obj in list_objs:
-                tmp_dict = cls.to_dictionary(obj)
-                list_dicts.append(tmp_dict)
-            json_L_of_D = cls.to_json_string(list_dicts)
+        """
+        the class method def save_to_file(cls, list_objs)
+
+        that writes the JSON string representation of list_objs to a file
+
+        list_objs is a list of instances who inherits of Base
+
+        example: list of Rectangle or list of Square instances
+
+        If list_objs is None, save an empty list
+
+        The filename must be: <Class name>.json - example: Rectangle.json
+
+        You must use the static method to_json_string (created before)
+
+        You must overwrite the file if it already exists
+        """
+        if list_objs is None:
+            data = cls.to_json_string(list_objs)
+            data = json.loads(data)
+            with open(f"{cls.__name__}.json", mode="w", encoding="utf-8") \
+                    as classfile:
+                json.dump(data, classfile)
+            return 1
         else:
-            json_L_of_D = "[]"
-
-        filename = cls.__name__ + ".json"
-
-        with open(filename, "w") as file:
-            num_char = file.write(json_L_of_D)
+            new_list_objs = []
+            for i in range(len(list_objs)):
+                hash_maps = list_objs[i].to_dictionary()
+                new_list_objs.append(hash_maps)
+            data = cls.to_json_string(new_list_objs)
+            data = json.loads(data)
+            with open(f"{cls.__name__}.json", mode="w", encoding="utf-8") \
+                    as classfile:
+                json.dump(data, classfile)
+            return 0
 
     @staticmethod
-    def to_json_string(list_dictionaries):
-        ''' method json string
-        '''
-        ret_list = []
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
+    def from_json_string(json_string):
+        """
+        the static method def from_json_string(json_string)
 
-        if type(list_dictionaries) != list:
-            ret_list.append(list_dictionaries)
+        that returns the list of the JSON string
+
+        representation json_string
+        """
+        if json_string is None:
+            return []
         else:
-            ret_list = list_dictionaries
-        return json.dumps(ret_list)
+            return json.loads(json_string)
 
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        ''' method json string
-        '''
-        ret_list = []
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        the class method def create(cls, **dictionary)
 
-        if type(list_dictionaries) != list:
-            ret_list.append(list_dictionaries)
+        that returns an instance with all attributes already set
+
+        **dictionary can be thought of as a double pointer to a dictionary
+
+        To use the update method to assign all attributes,
+
+        you must create a “dummy” instance before
+
+        Create a Rectangle or Square instance with “dummy”
+
+        mandatory attributes (width, height, size, etc.)
+
+        Call update instance method to this “dummy”
+
+        instance to apply your real values
+
+        You must use the method def update(self, *args, **kwargs)
+
+        **dictionary must be used as **kwargs of the method update
+
+        You are not allowed to use eval
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(3, 4)
+            update_dummy = dummy.update(**dictionary)
+            return dummy
         else:
-            ret_list = list_dictionaries
-        return json.dumps(ret_list)
+            dummy = cls(4)
+            update_dummy = dummy.update(**dictionary)
+            return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        the class method def load_from_file(cls)
+
+        that returns a list of instances
+
+        The filename must be: <Class name>.json - example: Rectangle.json
+
+        If the file doesn’t exist, return an empty list
+
+        Otherwise, return a list of instances -
+
+        the type of these instances depends on cls
+
+        (current class using this method)
+
+        You must use the from_json_string and
+
+        create methods (implemented previously)
+        """
+        try:
+            with open(f"{cls.__name__}.json", mode="r", encoding="utf-8") \
+                    as classfile:
+                new_object_list = []
+                data = classfile.read()
+                data = cls.from_json_string(data)
+                for i in range(len(data)):
+                    obj_data = data[i]
+                    obj = cls.create(**obj_data)
+                    new_object_list.append(obj)
+                return new_object_list
+        except FileNotFoundError:
+            return []
